998ac7cac239df24afd81cdacaaf4737af3342ff
Na21k <hulevych@hotmail.com>
2023-08-27 19:50:08 +0300

    migrate to Material 3

--- app/src/main/java/com/example/diyvocabulary/MainActivity.kt
+++ app/src/main/java/com/example/diyvocabulary/MainActivity.kt
@@ -11,15 +11,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setSupportActionBar(mBinding.appBar.appBar)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -29,7 +28,10 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        val navView: BottomNavigationView = mBinding.navView
         navView.setupWithNavController(navController)
     }
 }
