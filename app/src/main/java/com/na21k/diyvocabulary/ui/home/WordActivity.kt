1ed29c5ddaaff479b886fe20c415a598f3bf87ef
Na21k <hulevych@hotmail.com>
2023-09-17 00:07:35 +0300

    layout preparations for WordActivity

--- app/src/main/java/com/na21k/diyvocabulary/ui/home/WordActivity.kt
+++ app/src/main/java/com/na21k/diyvocabulary/ui/home/WordActivity.kt
@@ -1,14 +1,24 @@
 package com.na21k.diyvocabulary.ui.home
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.na21k.diyvocabulary.R
+import com.na21k.diyvocabulary.BaseActivity
+import com.na21k.diyvocabulary.databinding.ActivityWordBinding
 
 const val WORD_DOCUMENT_ID_ARG_KEY = "wordDocumentIdArgKey"
 
-class WordActivity : AppCompatActivity() {
+class WordActivity : BaseActivity() {
+
+    private lateinit var mBinding: ActivityWordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_word)
+
+        mBinding = ActivityWordBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        setSupportActionBar(mBinding.appBar.appBar)
+
+        //mViewModel = ...
+
+        enableUpNavigation(mBinding.appBar.appBar)
     }
 }
