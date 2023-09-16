87a425fcbf2a2dd805717b4798550f876bb43f3a
Na21k <hulevych@hotmail.com>
2023-09-16 22:49:23 +0300

    remove unused code from BaseFragment

--- app/src/main/java/com/na21k/diyvocabulary/BaseFragment.kt
+++ app/src/main/java/com/na21k/diyvocabulary/BaseFragment.kt
@@ -1,24 +1,9 @@
 package com.na21k.diyvocabulary
 
-import android.view.View
-import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 
 open class BaseFragment : Fragment() {
 
     protected open fun setListeners() {}
     protected open fun observeLiveData() {}
-
-    protected fun switchLoadingMode(isLoading: Boolean, progressBar: ProgressBar) {
-        if (isLoading) {
-            disableButtons()
-            progressBar.visibility = View.VISIBLE
-        } else {
-            enableButtons()
-            progressBar.visibility = View.GONE
-        }
-    }
-
-    protected open fun disableButtons() {}
-    protected open fun enableButtons() {}
 }
