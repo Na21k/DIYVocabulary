a57cf8b97ab52d56841e04b93eb707bcaaa6d0ec
Na21k <hulevych@hotmail.com>
2023-10-12 01:18:16 +0300

    implement search in HomeFragment
    (cherry picked from commit 5973a624ab19576dab595dc3a04aa4d4ab662cf8)

--- app/src/main/java/com/na21k/diyvocabulary/ui/home/HomeFragment.kt
+++ app/src/main/java/com/na21k/diyvocabulary/ui/home/HomeFragment.kt
@@ -3,11 +3,19 @@ package com.na21k.diyvocabulary.ui.home
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.snackbar.Snackbar
+import com.na21k.diyvocabulary.R
 import com.na21k.diyvocabulary.databinding.FragmentHomeBinding
 import com.na21k.diyvocabulary.model.WordModel
 import com.na21k.diyvocabulary.ui.MainActivitySharedViewModel
@@ -15,7 +23,7 @@ import com.na21k.diyvocabulary.ui.home.word.WORD_MODEL_ARG_KEY
 import com.na21k.diyvocabulary.ui.home.word.WordActivity
 import com.na21k.diyvocabulary.ui.shared.BaseFragment
 
-class HomeFragment : BaseFragment(), WordsListAdapter.OnWordActionListener {
+class HomeFragment : BaseFragment(), WordsListAdapter.OnWordActionListener, MenuProvider {
 
     private lateinit var mBinding: FragmentHomeBinding
     private lateinit var mViewModel: MainActivitySharedViewModel
@@ -32,6 +40,10 @@ class HomeFragment : BaseFragment(), WordsListAdapter.OnWordActionListener {
         savedInstanceState: Bundle?
     ): View {
         mBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val menuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.STARTED)
+
         return mBinding.root
     }
 
@@ -41,6 +53,24 @@ class HomeFragment : BaseFragment(), WordsListAdapter.OnWordActionListener {
         setListeners()
         observeLiveData()
     }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.home_fragment_options_menu, menu)
+
+        val searchItemActionView = menu.findItem(R.id.menu_search).actionView as SearchView
+        searchItemActionView.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean = false
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (!newText.isNullOrEmpty()) mListAdapter.search(newText)
+                else mListAdapter.clearSearch()
+
+                return false
+            }
+        })
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean = false
 
     private fun setUpRecyclerView(): WordsListAdapter {
         val rv = mBinding.wordsListRecyclerView
